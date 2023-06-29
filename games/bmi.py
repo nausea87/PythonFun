d24d427eb622b36a65fa742a4f5d22c882c9c784
Erik <62840687+nausea87@users.noreply.github.com>
2023-06-29 15:39:08 +0200

    added bmi calc

---
+++ games/bmi.py
@@ -0,0 +1,11 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+
+bmi = calculate_bmi(weight, height)
+
+print("Your BMI is: {:.2f}".format(bmi))
